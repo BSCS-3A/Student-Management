708f8bdca4b54ecad927cbddc89304d9d7535c53
ChristianDiesta <64411885+ChristianDiesta@users.noreply.github.com>
2021-03-15 14:39:54 +0800

    V2.0
    Adjusted student info based on the database

--- csv_connection/connect.php
+++ csv_connection/connect.php
@@ -1,36 +1,60 @@
 <?php
 
+//palitan nalang pag magconnect sa hosting site
 $dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
+$dbUsername = "root";   //id1621880_webhostingbscs3a
+$dbPassword = "";       //id16218880_buceils
 $dbName = "seproject";
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die ('Unable to connect');
+$connect = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die ('Unable to connect');
+$message = "";
 
-if(isset($_POST["import"])){
-    $fileName = $_FILES['file']['tmp_name'];
+$query = "SELECT * FROM student";
+$result = mysqli_query($connect, $query);
 
-    if($_FILES["file"]["size"] > 0){
-        $file = fopen($fileName, "r");
-        
-        //Adjust na lang sa data na gusto ng customer
-        while(($column = fgetcsv($file, 1000, ",")) !== FALSE){
-            $sql_insert = "INSERT into stud_info (student_id, first_name, last_name, gender, email) values 
-            ('" . $column[0] . "', 
-             '" . $column[1] . "', 
-             '" . $column[2] . "',
-             '" . $column[3] . "', 
-             '" . $column[4] . "')"; 
+if(isset($_POST["upload"])){
+    if($_FILES['info_file']['name']){
+        $filename = explode(".", $_FILES['info_file']['name']);
+        if(end($filename) == "csv"){
+            $handle = fopen($_FILES['info_file']['tmp_name'], "r");
+            while($data = fgetcsv($handle)){
+                $student_id = mysqli_real_escape_string($connect, $data[0]);
 
-            $result = mysqli_query($conn, $sql_insert);
+                $fname = mysqli_real_escape_string($connect, $data[1]);
 
-            if(!empty($result)){
-                echo "CSV Data Imported into the database";
+                $Mname = mysqli_real_escape_string($connect, $data[2]);
+
+                $lname = mysqli_real_escape_string($connect, $data[3]);
+
+                $gender = mysqli_real_escape_string($connect, $data[4]);
+
+                $bumail = mysqli_real_escape_string($connect, $data[5]);
+
+                $grade_level = mysqli_real_escape_string($connect, $data[6]);
+
+                $otp = mysqli_real_escape_string($connect, $data[7]);
+
+                $voting_status = mysqli_real_escape_string($connect, $data[8]);
+
+                $query = " UPDATE student SET fname = '$fname', Mname = '$Mname', gender = '$gender', bumail = '$bumail', grade_level = '$grade_level', otp = '$otp', voting_status = '$voting_status' WHERE student_id = '$student_id'";
+
+                mysqli_query($connect, $query);
             }
-            else{
-                echo "Problem in importing csv";
-            }
+            fclose($handle);
+            header("location: StudentAccountManagement.php>updation=1");
+        }
+        else{
+            $message = '<label class="text-danger">Please Select CSV File</label>';
         }
     }
+    else{
+        $message = '<label class="text-danger">Please Select File</label>';
+    }
 }
+
+if(isset($_GET["updation"])){
+    $message = '<label class="text-success">File Updated</label>';
+}
+
+
 ?>
